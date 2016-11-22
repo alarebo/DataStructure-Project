019d97583ad42d9a818071403db0386c0bfa8ea1
Amanuel Larebo <alarebo@gmail.com>
2016-11-22 16:38:18 -0500

    Update WorkOutSelection.java

--- WorkOutSelection.java
+++ WorkOutSelection.java
@@ -1,215 +1,159 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Image;
-import java.awt.Insets;
 import java.awt.event.ActionListener;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.BorderFactory;
+import javax.swing.JTabbedPane;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+@SuppressWarnings("serial")
+public class WorkOutSelection extends JFrame{
+	JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 
-public class WorkOutSelection extends JFrame
-{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private static final int FRAME_WIDTH = 900;
-	private static final int FRAME_HEIGHT = 400;
-	
-	private JLabel Icon1,Icon2,Icon3,Icon4,choose;	 
-	private ImageIcon WorkOut;
-	private JButton Legs,Arms,Core,cardio,Full;
-
-	public WorkOutSelection()
-	 { 		 
-		createButton();
-		createJLabel();
+	public WorkOutSelection() {
+		setTitle("Workout Seclection");
+		
+		setSize(789,460);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		EqipmentPanel();
 		createPanel();
-		setSize(FRAME_WIDTH, FRAME_HEIGHT);
-		setBackground(Color.blue);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 }
 
-	private void createButton() 
+	}
+	private void EqipmentPanel() 
 	{
-		Legs = new JButton("Legs");
-		Legs.setPreferredSize(new Dimension(100, 50));
-		Legs.setBackground(Color.RED);
-		Legs.setOpaque(true);
-		  
-		Arms = new JButton("Arms");
-		Arms.setPreferredSize(new Dimension(100, 50));
-		Arms.setBackground(Color.RED);
-		Arms.setOpaque(true);
-		
-
-		Core = new JButton("Core");
-		Core.setPreferredSize(new Dimension(100, 50));
-		Core.setBackground(Color.RED);
-		Core.setOpaque(true);
-		
-		
-		Full = new JButton("Full Body");
-		Full.setPreferredSize(new Dimension(120, 50));
-		Full.setBackground(Color.RED);
-		Full.setOpaque(true);
-		
-		
-		cardio = new JButton("Cardio");
-		cardio.setPreferredSize(new Dimension(120, 50));
-		cardio.setBackground(Color.RED);
-		cardio.setOpaque(true);
-		  
-		// Inizilising ActionListners
-		ActionListener listener = new BTNlistener();
-		ActionListener listener1 = new BTNlistener1();
-		ActionListener listener2 = new BTNlistener2();
-		ActionListener listener3 = new BTNlistener3();
-
-		//add the Action listner to the gui
-		Legs.addActionListener(listener);
-		Arms.addActionListener(listener1); 
-		Core.addActionListener(listener2); 
-		Full.addActionListener(listener3);	
-	  }
-	 class BTNlistener implements ActionListener
-	 {
-		 public void actionPerformed(ActionEvent event)
-		 {
-			 
-		 } 
-	 
-	 }
-	 
-	 class BTNlistener1 implements ActionListener
-	 {	 
-		 public void actionPerformed(ActionEvent event)	  
-		 {
-			 
-		 } 	 
-	 }
-	 class BTNlistener2 implements ActionListener
-	 {	 
-		 public void actionPerformed(ActionEvent event)	  
-		 {
-			 
-		 } 	 
-	 }
-	 class BTNlistener3 implements ActionListener
-	 {	 
-		 public void actionPerformed(ActionEvent event)	  
-		 {
-			 
-		 } 	 
-	 }
-
-	private void createJLabel()
-	{
-		//Image img = new ImageIcon("resources/leg.png").getImage().getScaledInstance(80, 50, java.awt.Image.SCALE_SMOOTH);//Getting image from workspace src folder//
-		ImageIcon img2 = new ImageIcon("resources/arm.gif");
-		Image img3 = new ImageIcon("resources/w.png").getImage().getScaledInstance(80, 40, java.awt.Image.SCALE_SMOOTH);
-
-
-		WorkOut = new ImageIcon("resources/pushUP.gif");
-		ImageIcon core1 = new ImageIcon("resources/leg.gif");
-		ImageIcon c = new ImageIcon("resources/cardio.gif");
-		ImageIcon d = new ImageIcon(img3);
-
-
-
-		Icon1 = new JLabel();        //Creating Label//		
-		Icon2 = new JLabel();		 
-		Icon3 = new JLabel();
-		Icon4 = new JLabel();
-		choose = new JLabel();		
-		choose.setForeground(Color.RED);
-		
-		Icon1.setIcon(core1);       //Setting up Icon for Labels//
-		Icon2.setIcon(img2);
-		Icon3.setIcon(WorkOut);
-		Icon4.setIcon(c);
-		choose.setIcon(d);
-		
-
 
 		
 	}
-
 	private void createPanel() 
 	{
-		JPanel pan = new JPanel();
-        JPanel panel = new JPanel();
-        
-		GridBagConstraints gbc= new GridBagConstraints();
-		panel.setLayout(new GridBagLayout());
-		 
-		panel.setBackground(Color.CYAN);
-		pan.setBackground(Color.blue);
+		
+		Image img =  new ImageIcon("resources/bckground_workout.jpg").getImage().getScaledInstance(607, 373,
+		        Image.SCALE_SMOOTH);
 
-		 
-		gbc.insets = new Insets(5,5,5,5);
-		 
-		gbc.gridx = 0;
-		gbc.gridx = 0;
-		  
-		panel.add(choose,gbc);
-		 
-		gbc.gridy = 0;
-		gbc.gridy = 1;
 
-		panel.add(Full,gbc);
-		 
-		gbc.gridx = 1;
-		gbc.gridy = 0;
+
+		ImageIcon legIcon = new ImageIcon("resources/leg.gif");
+		ImageIcon armIcon = new ImageIcon("resources/arm.gif");
+		ImageIcon coreIcon = new ImageIcon("resources/pushUp.gif");
+		ImageIcon cardioIcon = new ImageIcon("resources/cardio.gif");
+
 		
-		panel.add(Legs, gbc);
-		 
-		gbc.gridx = 2;
-		gbc.gridy = 0;
-		 
-		panel.add(Arms,gbc); 
-		  
-		gbc.gridx = 3;
-		gbc.gridy = 0;
-		  
-		panel.add(Core,gbc);
-		 
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		 
-		panel.add(Icon1,gbc);
+		ImageIcon icon = new ImageIcon(img);
 		
-		gbc.gridx = 4;
-		gbc.gridy = 0;
-		panel.add(cardio);
-		 
-		gbc.gridx = 2;
-		gbc.gridy = 1;
-		  
-		panel.add(Icon2,gbc);
-		  
-		gbc.gridx = 3;
-		gbc.gridy = 1;
 		
-		panel.add(Icon3,gbc);
+		JPanel eqpPanel = new JPanel();
+		tabbedPane.addTab("New tab", null, eqpPanel, null);
+		eqpPanel.setLayout(null);
 		
-		gbc.gridx = 4;
-		gbc.gridy = 1;
+		JButton leg = new JButton("Legs");
+		leg.setBounds(93, 13, 83, 25);
+		eqpPanel.add(leg);
 		
-		panel.add(Icon4,gbc);
+		JButton arm = new JButton("Arms");
+		arm.setBounds(185, 13, 83, 25);
+		eqpPanel.add(arm);
 		
-		panel.setBorder(BorderFactory.createLineBorder(Color.blue));
-		pan.add(panel,BorderLayout.CENTER);
-		add(pan);
+		JButton cardio = new JButton("Cardio");
+		cardio.setBounds(280, 13, 97, 25);
+		eqpPanel.add(cardio);
+		
+		JButton coreW = new JButton("Core");
+		coreW.setBounds(395, 13, 97, 25);
+		eqpPanel.add(coreW);
+		
+		JButton fullB = new JButton("Full Workout");
+		fullB.setBounds(12, 74, 83, 25);
+		eqpPanel.add(fullB);
+		
+		JButton button_5 = new JButton("New button");
+		button_5.setBounds(12, 127, 83, 25);
+		eqpPanel.add(button_5);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(103, 47, 423, 2);
+		eqpPanel.add(separator_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setLayout(null);
+		panel_3.setBounds(103, 51, 423, 188);
+		eqpPanel.add(panel_3);
+		
+		
+		//
+		
+		//leg.setIcon(legIcon);
+		//arm.setIcon(armIcon);
+		//coreW.setIcon(coreIcon);
+		//cardio.setIcon(cardioIcon);
+		
+		
+		
+		
+		
+		//first tabbed panel
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("With Equipment", null, panel, null);
+		panel.setLayout(null);
+		
+		JButton Leg = new JButton("Legs");
+		Leg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				
+			}
+		});
+		Leg.setBounds(150, 23, 83, 25);
+		panel.add(Leg);
+		
+		JButton Arms = new JButton("Arms");
+		Arms.setBounds(270, 23, 118, 25);
+		panel.add(Arms);
+		
+		JButton Cardio = new JButton("Cardio");
+		Cardio.setBounds(432, 23, 97, 25);
+		panel.add(Cardio);
+		
+		JButton Core = new JButton("Core");
+		Core.setBounds(600, 23, 97, 25);
+		panel.add(Core);
+		
+		JButton Full = new JButton("Full Workout");
+		Full.setBounds(12, 65, 107, 25);
+		panel.add(Full);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(12, 159, 107, 25);
+		panel.add(btnNewButton_1);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(131, 50, 582, 199);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(147, 0, 135, 175);
+		panel_1.add(label);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setBounds(294, 0, 135, 175);
+		panel_1.add(label_1);
+		
+		JLabel label_2 = new JLabel("");
+		label_2.setBounds(447, 0, 135, 175);
+		panel_1.add(label_2);
+		
+		
+		JLabel legL = new JLabel("");
+		legL.setBounds(12, 0, 135, 175);
+		panel_1.add(legL);
+		legL.setIcon(legIcon);
+		getContentPane().add(tabbedPane);
 	}
-
 	public static void main(String[] args)
 	{
 	    WorkOutSelection frame = new WorkOutSelection();
@@ -220,6 +164,4 @@ public class WorkOutSelection extends JFrame
 
 	    frame.setVisible(true);
 	}
-	
-
 }
