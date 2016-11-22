098ce1d074cbf45c494103607c460734535a99fa
Amanuel Larebo <alarebo@gmail.com>
2016-11-22 16:20:12 -0500

    Update UserLogin.java

--- UserLogin.java
+++ UserLogin.java
@@ -10,15 +10,15 @@ public class UserLogin extends JFrame
 	private static final int FRAME_WIDTH = 400;
 	private static final int FRAME_HEIGHT = 200;
 	private JButton ulogin,newUsr;
-	private JLabel userN,passWD;
+	private JLabel userN,passWD,errorLabel;
 	private JTextField txtuser;
 	private JPasswordField pass;
 	JPanel panel = new JPanel();
+	private JLabel bcLabel;
 	
 	public static void main(String[] args)
 	{
 	   UserLogin logframe = new  UserLogin();
-	   logframe.setTitle("User Authentication");
 	   logframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	   logframe.setVisible(true);
 	}
@@ -26,6 +26,7 @@ public class UserLogin extends JFrame
 	
 	public UserLogin()
 	{
+		setTitle("User Authentication");
 		creatCompenent();
 		createPanel();
 		setSize(FRAME_WIDTH,FRAME_HEIGHT);
@@ -35,26 +36,33 @@ public class UserLogin extends JFrame
 
 	private void creatCompenent() 
 	{
+
 		txtuser =  new JTextField(15);
+		txtuser.setBounds(132, 29, 171, 22);
 		pass =  new JPasswordField(15);
+		pass.setBounds(132, 58, 171, 22);
 		
 		ulogin = new JButton("Login");
+		ulogin.setBounds(208, 120, 145, 20);
 		ulogin.setPreferredSize(new Dimension(145, 20));
 
 		newUsr = new JButton("Create Account");
+		newUsr.setBounds(43, 120, 145, 20);
 		newUsr.setPreferredSize(new Dimension(145, 20));
 
 		userN = new JLabel("User Name:");
+		userN.setBounds(56, 32, 68, 16);
 		passWD  = new JLabel("Password:");
-		
-		userN.setForeground(Color.RED);
-		passWD.setForeground(Color.RED);
+		passWD.setBounds(56, 61, 68, 16);
 		
 		ActionListener lg =  new lgnlistener();
 		ulogin.addActionListener(lg);
 		
 		ActionListener ct =  new CreateListener();
 		newUsr.addActionListener(ct);
+		
+        userN.setForeground(Color.WHITE);
+        passWD.setForeground(Color.WHITE);
 
 	}
 	class CreateListener implements ActionListener
@@ -86,7 +94,10 @@ public class UserLogin extends JFrame
 		      } 
 		      else 
 		      {
-		        JOptionPane.showMessageDialog(null,"Wrong Password / Username");
+		        errorLabel.setText("Wrong Password / Username");	
+		        errorLabel.setForeground(Color.RED);
+
+		        
 		        txtuser.setText("");
 		        pass.setText("");
 		        txtuser.requestFocus();
@@ -97,40 +108,28 @@ public class UserLogin extends JFrame
 	}
 	private void createPanel()
 	{
+		Image img =  new ImageIcon("resources/background.jpg").getImage().getScaledInstance(382, 153,
+		        Image.SCALE_SMOOTH);
+		ImageIcon icon = new ImageIcon(img);
 		JPanel panel = new JPanel();
-		GridBagConstraints gbc= new GridBagConstraints();
-		panel.setLayout(new GridBagLayout());
-
-		gbc.insets = new Insets(5,5,5,5);
+		panel.setLayout(null);
+		panel.add(userN);
+		panel.add(passWD);
+		panel.add(txtuser);
+		panel.add(pass);
+		panel.add(ulogin);
+		panel.add(newUsr);
 		
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		  
-		panel.add(userN,gbc);
-		 
-		gbc.gridy = 0;
-		gbc.gridy = 1;
+		getContentPane().add(panel);
 		
-		panel.add(passWD,gbc);
+	    errorLabel = new JLabel("");
+		errorLabel.setBounds(66, 90, 248, 17);
+		panel.add(errorLabel);
 		
-		gbc.gridx = 5;
-		gbc.gridy = 0;
-		panel.add(txtuser,gbc);
-		
-		gbc.gridx = 5;
-		gbc.gridy = 1;
-		panel.add(pass,gbc);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 3;
-		panel.add(ulogin,gbc);
-		
-		gbc.gridx = 5;
-		gbc.gridy = 3;
-		panel.add(newUsr,gbc);
-		
-		add(panel);
+		bcLabel = new JLabel("");
+		bcLabel.setIcon(icon);
+		bcLabel.setBounds(0, 0, 382, 153);
+		panel.add(bcLabel);
 
 	}
-
 }
