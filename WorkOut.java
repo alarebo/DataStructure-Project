61dc705abe9c184e75ec47311ff52a50b7ebbac1
Amanuel Larebo <alarebo@gmail.com>
2016-11-22 16:40:29 -0500

    Update WorkOut.java

--- WorkOut.java
+++ WorkOut.java
@@ -1,123 +1,178 @@
-import sun.audio.*;
-//http://stackoverflow.com/questions/11919009/using-javax-sound-sampled-clip-to-play-loop-and-stop-mutiple-sounds-in-a-game
-import java.io.FileInputStream;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
-import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
-@SuppressWarnings("serial")
-public class WorkOut extends JFrame
-{
-	String song = "";
-	private JLabel animation;
-	private JButton play,stop;
-	boolean start =false;
-	
-	public static void main(String [] args)
-	{
-		WorkOut f =  new WorkOut();
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	/**
+	 * Handles playing, stoping, and looping of sounds for the game.
+	 * @author Tyler Thomas
+	 *
+	 */
+	@SuppressWarnings("serial")
+	public class WorkOut extends JFrame {
+		static int st=0;
+		static String tune = "resources/Ms_New_Booty_Lyrics.wav";
+		public static void main(String[] args) 
+		{
+			WorkOut frame = new WorkOut();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+			frame.play();
+	    }
+		static class stpl implements ActionListener
+		 {
+			 public void actionPerformed(ActionEvent event)
+			 {	
+				 clip.setFramePosition(0);  // Must always rewind!
+				 clip.start();
+			 } 
+		 
+		 }
+		static class stspl implements ActionListener
+		 {
+			 public void actionPerformed(ActionEvent event)
+			 {	
+				 if(clip.isActive())
+				 {
+					clip.stop(); 
+				 }
+			 } 
+		 
+		 }
 
-	}
-	public WorkOut()
-	{
-		createGui();   
-		createGui();
-		createPanel();
-		
-	   	setTitle(" Work Out");
-        setSize(500,500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        createGui();   
-		createGui();
-		createPanel();
-	}
+		public static void open()
+		{
+			music(tune);	
+		}
+	    private static Clip clip;
+	    public WorkOut() 
+	    {
+	    	createGui();
 
-	private void createGui() 
-	{
-		play =  new JButton("Play");
-		stop = new JButton("Stop");
-		ActionListener pl = new startListner();
-		ActionListener st = new stopListner();
-		play.addActionListener(pl);
-		stop.addActionListener(st);
-		animation = new JLabel();
-		
-		ImageIcon img =  new ImageIcon("resources/leg.jpg");
-		animation.setIcon(img);
-	}
-	class startListner implements ActionListener
-	{
-		public void actionPerformed(ActionEvent event)
-		{
-			start =true;
-			music();
-		}
-	}
-	class stopListner implements ActionListener
-	{
-		public void actionPerformed(ActionEvent event)
-		{
-			start = false;
-			music();
-		}
-	}
-	@SuppressWarnings("restriction")
-	public void music()
-	{
-		AudioPlayer player = AudioPlayer.player;
-		AudioStream stream;
-		AudioData audio;
-		ContinuousAudioDataStream loop = null;
-		
-		try
-		{
-			stream =  new AudioStream(new FileInputStream(song));
-			audio = stream.getData();
-			loop = new ContinuousAudioDataStream(audio);
-			if(start=false)
-			{
-				player.stop(loop);				
-			}
-			else
-			{
-				player.start(loop);
-			}
-		}
-		catch(IOException error)
-		{
+	    	setSize(550,450);
+			setTitle("Leg WorkOut");
+			setVisible(true);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+			open();
+			st=0;
+
+	    }
+	    
+	    public static  void music(String fileName) {	      
+	    	
+	    	try 
+	    	{
+	    		File file = new File(fileName);
+	    		if (file.exists()) 
+	    		{
+	    			AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+	    			// load the sound into memory (a Clip)
+	    			clip = AudioSystem.getClip();
+	    			clip.open(sound);
+	    			if(st ==0){clip.start();}
+	    		}
+	    		else
+	    		{
+	    			throw new RuntimeException("Sound: file not found: " + fileName);
+	    		}
+	    	}
+       
+	    	catch (MalformedURLException e) 
+	    	{
+	    		e.printStackTrace();
+	    		throw new RuntimeException("Sound: Malformed URL: " + e);
+	    	}
+    
+	    	catch (UnsupportedAudioFileException e) 
+	    	{         
+	    		e.printStackTrace();
+	    		throw new RuntimeException("Sound: Unsupported Audio File: " + e);   
+	    	}
+	    	catch (IOException e)
+	    	{           
+	    		e.printStackTrace();        
+	    		throw new RuntimeException("Sound: Input/Output Error: " + e);
+	    	}
+	    	catch (LineUnavailableException e) 
+	    	{
+	    		e.printStackTrace();
+	    		throw new RuntimeException("Sound: Line Unavailable Exception Error: " + e);
+	    	}
 			
 		}
-		
-	}
-	private void createPanel() 
-	{
-		JPanel panel = new JPanel();
-        
-		GridBagConstraints gbc= new GridBagConstraints();
-		panel.setLayout(new GridBagLayout());
-		 
-		panel.setBackground(Color.CYAN);
-		 
-		gbc.insets = new Insets(5,5,5,5);
-		 
-		gbc.gridx = 0;
-		gbc.gridx = 0;
-		  
-		panel.add(play,gbc);
-		 
-		gbc.gridy = 0;
-		gbc.gridy = 1;
 
-		panel.add(stop,gbc);
-		
-		add(panel);
-	}
+		private void createGui() {
+			
+			Image img =  new ImageIcon("resources/bckground_workout.jpg").getImage().getScaledInstance(550, 405,
+			        Image.SCALE_SMOOTH);
+			ImageIcon ico = new ImageIcon(img);
+			
+	    	JButton start =  new JButton("Start");
+	    	start.setBounds(319, 326, 75, 25);
+			JButton Stop =  new JButton("stop");
+			Stop.setBounds(239, 326, 61, 25);
+			JLabel animation = new JLabel();
+			
+			ActionListener l =  new stpl();
+			start.addActionListener(l);
+			ActionListener s =  new stspl();
+			Stop.addActionListener(s);
+			
+			
+			ImageIcon c = new ImageIcon("resources/squats.gif");
+			
+			animation.setIcon(c);
+			
+			JPanel panel = new JPanel();
+			panel.setLayout(null);
+	        
+			GridBagConstraints gbc= new GridBagConstraints();
+			
 
-}
+			gbc.insets = new Insets(5,5,5,5);	 
+			
+			panel.add(Stop);
+				 
+			panel.add(start);
+			
+			getContentPane().add(panel,BorderLayout.CENTER);
+			
+			JButton btnBack = new JButton("back");
+			btnBack.setBounds(149, 326, 75, 25);
+			panel.add(btnBack);
+			
+			JPanel panel_1 = new JPanel();
+			panel_1.setBounds(149, 48, 228, 263);
+			panel.add(panel_1);
+			
+			JLabel animationE = new JLabel();
+			panel_1.add(animationE);
+			animationE.setIcon(c);
+			
+			JLabel bgL = new JLabel("");
+			bgL.setIcon(ico);
+			bgL.setBounds(0, 0, 550, 405);
+			panel.add(bgL);
+		}
+		public void play(){
+	        clip.start();
+	    }
+	 }
