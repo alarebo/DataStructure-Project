9f6d32a73d789162a242d684c6812c86af9f0728
Amanuel Larebo <alarebo@gmail.com>
2016-11-26 01:30:55 -0500

    Update NewUser.java

--- NewUser.java
+++ NewUser.java
@@ -2,25 +2,26 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 @SuppressWarnings("serial")
 public class NewUser extends JFrame {
 	
 	Image ic = new ImageIcon("resources/background.jpg").getImage().getScaledInstance(80, 40, java.awt.Image.SCALE_SMOOTH);
 	ImageIcon bcGround = new ImageIcon(ic);
+	String e = " ";
+	boolean isEmpty = false;
 
-	String gender;
-	public static void main(String[] args) 
-	{
-		NewUser frame = new NewUser();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	String gender="n/a",names;
+	int count;
+	
+	//Error Label error message is shown when one of the texfield is empty
+	JLabel errorLabel;
 
-    }
 	 
 	JTextField name = new JTextField(15);
     JTextField weight = new JTextField(5);
     JTextField height = new JTextField(5);
-    JTextField age = new JTextField(15);
     JTextField in = new JTextField(15);
 
     JTextField username = new JTextField(15);
@@ -30,7 +31,6 @@ public class NewUser extends JFrame {
     JLabel passL = new JLabel("Password:");
     JLabel heightL = new JLabel("Hieght");
     JLabel nameL = new JLabel("Name:");
-    JLabel ageL = new JLabel("Age:");
     JLabel ft = new JLabel("ft");
     JLabel inl = new JLabel("in");	  
 
@@ -41,17 +41,23 @@ public class NewUser extends JFrame {
     JRadioButton male = new JRadioButton("Male", false);
     JRadioButton female = new JRadioButton("Female", false);
     
+    public static void main(String[] args) 
+	{
+		NewUser frame = new NewUser();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
  
-    
    private JPanel panel = new JPanel();
 	
 	//Creating GUI FOR USER info
-    NewUser()
-    {     
-    	super("Create Account");
+    public NewUser()
+    {    
+    	setTitle("Create Account");
         setSize(500,500);
         setLocation(500,480);
         setVisible(true);
+        panel.setForeground(Color.RED);
         panel.setLayout (null); 
         getContentPane().add(panel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -83,6 +89,11 @@ public class NewUser extends JFrame {
         JSeparator separator = new JSeparator();
         separator.setBounds(10, 202, 220, 35);
         panel.add(separator);
+        
+        errorLabel = new JLabel("");
+        errorLabel.setForeground(Color.RED);
+        errorLabel.setBounds(42, 3, 295, 20);
+        panel.add(errorLabel);
         panel.repaint();
 
 
@@ -92,26 +103,27 @@ public class NewUser extends JFrame {
 	private void createGui()
 	{
 		//setting the position of gui
-	    name.setBounds(58,32,150,20);
+	    name.setBounds(58,30,158,20);
 	    username.setBounds(90,65,126,20);
-	    passWord.setBounds(292,65,120,20);
+	    passWord.setBounds(330,65,120,20);
 	    weight.setBounds(66,100,150,20);
 	    height.setBounds(58,125,40,20);
 	    in.setBounds(128,125,40,20);
 	    ft.setBounds(103,125,40,20);
 	    inl.setBounds(173,125,40,20);
 
-	    bmi.setBounds(155,150,75,20);
+	    bmi.setBounds(155,150,61,20);
 	    bmiL.setBounds(15,150,140,20);
-	    bmical.setBounds(249,150,105,20);
+	    bmical.setBounds(266,150,120,20);
 	    weightL.setBounds(15,100,150,20);
 	    heightL.setBounds(15,125,51,20);
 	    nameL.setBounds(15,30,150,20);
 	    userL.setBounds(10,65,80,20);
-	    passL.setBounds(228,65,69,20);
+	    passL.setBounds(266,65,69,20);
 	    create.setBounds(15,250,150,20);
 	    ActionListener usr = new userInfo();
 	    create.addActionListener(usr);
+
 	    
 	    ActionListener GEN = new genderLST2();
 	    male.addActionListener(GEN);
@@ -141,6 +153,7 @@ public class NewUser extends JFrame {
 		//calculate the users Body Mass index
 		public void actionPerformed(ActionEvent event)	
 		{
+			
 		    String wieght = weight.getText();
 		    String i = in.getText();
 		    double inch = Double.parseDouble(i);
@@ -163,25 +176,74 @@ public class NewUser extends JFrame {
 			}
 		}
 	}
-	 class userInfo implements ActionListener
-	{
-		public void actionPerformed(ActionEvent e)
+
+	class userInfo implements ActionListener
+	{   //calls the isEmpty boolean to check if the form is filled and pushes info into database
+
+		 public void actionPerformed(ActionEvent e)
 		//user info is entered into the database
 		{
-			//String Name = name.getText();
-			//String UserName = username.getText();
-			//String PassWord = new String(passWord.getPassword());
-			UserLogin lg = new UserLogin();
-			lg.setVisible(true);
-			dispose();
-			JOptionPane.showMessageDialog(null,"account Created "+gender);
-
-			
-			
-		}
+			 count =0;
+		         iEmpty();
+			 if(count ==2)
+			 {
+				 errorLabel.setText("Error: one or more field entered are incorrect");
+			 }
+			 if(count ==0)
+			 {
+				 names = name.getText();
+				 //String UserName = username.getText();	
+				 //String PassWord = new String(passWord.getPassword());
+				 UserLogin lg = new UserLogin();
+				 lg.setVisible(true);
+				 dispose();
+				 JOptionPane.showMessageDialog(null,"account Created "+names);
+			 }
+		 }
 		
 	}
-	 
+	public int iEmpty()
+	{
+		int i = 0; 
+		String b = bmi.getText();
+		if(i==10){}
+		else if(height.getText() =="0"||height.getText().isEmpty()==true)
+		{
+			return count =2;		
+		}
+		else if(weight.getText().equals("0")||weight.getText().isEmpty()==true)
+		{
+			return count =2;
+		}
+		else if(passWord.getPassword().length ==0)
+		{
+			return count = 2;
+		}
+		
+		else if(b.equals("0")||b.isEmpty()==true)
+		{
+			errorLabel.setText("Error: one or more field entered are incorrect");
+			return count = 2;
+
+		}
+		else if(username.getText().isEmpty()==true)
+		{
+			errorLabel.setText("Error: one or more field entered are incorrect");
+			return count = 2;
+		}
+		else if(name.getText().isEmpty()==true)
+		{
+			errorLabel.setText("Error: one or more field entered are incorrect");
+			return count = 2;
+		}
+		else if(!(count==2))
+		{
+			return count = 0;
+		}
+		return i ;
+	
+	}
+	
 	 class genderLST implements ActionListener
 	 {
 		 public void actionPerformed(ActionEvent e)
